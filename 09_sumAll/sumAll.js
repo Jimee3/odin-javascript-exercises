41b5a760781d9c60ec0e81b0a0349d3658218ab3
Jimee3 <220510844+Jimee3@users.noreply.github.com>
2026-04-08 22:30:13 +0100

    create function to sum all the numbers between two positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    if (a != Math.floor(a) || b != Math.floor(b)) return "ERROR";
+    
+    let start
+    // the if statement checks which parameter is bigger
+    if (b < a) {
+        start = b;
+        end = a
+    } else {
+        start = a;
+        end = b
+    }
 
+    for (let i = start + 1; i <= end; i++){
+        start += i;
+    }
+    return start;
 };
 
 // Do not edit below this line
